426454da06c791746ebcd881c0c8fd01e7c76969
David <espabastidas19@gmail.com>
2018-12-16 18:20:07 -0500

    inicial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Usuarios;
+use App\Delegacion;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $usuarios = Usuarios::orderBy('nombre')->get();
+      $delegaciones = Delegacion::all();
+      return view('home', [
+          'usuarios' => $usuarios,
+          'delegaciones' => $delegaciones,
+      ]);
+    }
+}
