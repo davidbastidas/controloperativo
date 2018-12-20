1b5b574eb3fe6e5e455c3175e80e67308c84dc3d
David <espabastidas19@gmail.com>
2018-12-19 22:22:22 -0500

    build api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Avisos;
+use App\Auditoria;
+use App\Pci;
 use App\Anomalias;
-use App\Resultados;
-use App\EntidadesPagos;
 use App\ObservacionesRapidas;
 use App\Usuarios;
 use App\Log;
@@ -37,24 +36,23 @@ class ApiController extends Controller
 
     return $response;
   }
-  public function getAvisos(Request $request)
+  public function getServicios(Request $request)
   {
-    $arrayAvisos = [];
+    $arrayAuditorias = [];
     $arrayAnomalias = [];
-    $arrayResultados = [];
-    $arrayEntidades = [];
+    $arrayPci = [];
     $arrayObservacionesRapidas = [];
     $arrayFINAL = [];
     $collection = null;
 
     $fechaHoy = Carbon::now()->format('Y-m-d');
-    $avisos = Avisos::select('agenda_id')
-                    ->where('gestor_id', '=', $request->user)
+    $auditorias = Auditoria::select('agenda_id')
+                    ->where('lector_id', '=', $request->user)
                     ->where('estado', '=', '1')
                     ->groupBy('agenda_id')->get();
     $agendas = Agenda::where('fecha', '<=', "'$fechaHoy'");
-    foreach ($avisos as $aviso) {
-      $agendas = $agendas->orWhere('id', $aviso->agenda_id);
+    foreach ($auditorias as $auditoria) {
+      $agendas = $agendas->orWhere('id', $auditoria->agenda_id);
     }
     $agendas = $agendas->get();
 
@@ -63,25 +61,63 @@ class ApiController extends Controller
       $arrayIn[] = $agenda->id;
     }
 
-    $avisos = Avisos::where('gestor_id', '=', $request->user)
+    $auditorias = Auditoria::where('lector_id', '=', $request->user)
                     ->where('estado', '=', '1')
                     ->whereIn('agenda_id', $arrayIn)->get();
-    foreach ($avisos as $aviso) {
-      array_push($arrayAvisos, (object) array(
-        'id' => $aviso->id,
-        'tipo_visita' => $aviso->tipo_visita,
-        'municipio' => $aviso->municipio,
-        'localidad' => $aviso->localidad,
-        'barrio' => $aviso->barrio,
-        'direccion' => $aviso->direccion,
-        'cliente' => $aviso->cliente,
-        'deuda' => $aviso->deuda,
-        'factura_vencida' => $aviso->factura_vencida,
-        'nic' => $aviso->nic,
-        'nis' => $aviso->nis,
-        'medidor' => $aviso->medidor,
-        'tarifa' => $aviso->tarifa,
-        'fecha_limite_compromiso' => $aviso->compromiso
+    foreach ($auditorias as $auditoria) {
+      array_push($arrayAuditorias, (object) array(
+        'id' => $auditoria->id,
+        'barrio' => $auditoria->barrio,
+        'localidad' => $auditoria->localidad,
+        'cliente' => $auditoria->cliente,
+        'direccion' => $auditoria->direccion,
+        'nic' => $auditoria->nic,
+        'ruta' => $auditoria->ruta,
+        'itin' => $auditoria->itin,
+        'medidor' => $auditoria->medidor,
+        'motivo' => $auditoria->motivo,
+        'nis' => $auditoria->nis,
+        'pide_gps' => $auditoria->pide_gps
+      ));
+    }
+
+    //buscando Pci
+    $pcis = Pci::select('agenda_id')
+                    ->where('lector_id', '=', $request->user)
+                    ->where('estado', '=', '1')
+                    ->groupBy('agenda_id')->get();
+    $agendas = Agenda::where('fecha', '<=', "'$fechaHoy'");
+    foreach ($pcis as $pci) {
+      $agendas = $agendas->orWhere('id', $pci->agenda_id);
+    }
+    $agendas = $agendas->get();
+
+    $arrayIn = array();
+    foreach ($agendas as $agenda) {
+      $arrayIn[] = $agenda->id;
+    }
+
+    $pcis = Pci::where('lector_id', '=', $request->user)
+                    ->where('estado', '=', '1')
+                    ->whereIn('agenda_id', $arrayIn)->get();
+    foreach ($pcis as $pci) {
+      array_push($arrayPci, (object) array(
+        'id' => $pci->id,
+        'ct' => $pci->ct,
+        'mt' => $pci->mt,
+        'direccion' => $pci->direccion,
+        'medidor' => $pci->medidor,
+        'medidor_anterior' => $pci->medidor_anterior,
+        'medidor_posterior' => $pci->medidor_posterior,
+        'barrio' => $pci->barrio,
+        'municipio' => $pci->municipio,
+        'codigo' => $pci->codigo,
+        'an_anterior' => $pci->an_anterior,
+        'lectura_anterior' => $pci->lectura_anterior,
+        'unicom' => $pci->unicom,
+        'ruta' => $pci->ruta,
+        'itin' => $pci->itin,
+        'pide_gps' => $pci->pide_gps
       ));
     }
 
@@ -89,23 +125,10 @@ class ApiController extends Controller
     foreach ($anomalias as $anomalia) {
       array_push($arrayAnomalias, (object) array(
         'id' => $anomalia->id,
-        'nombre' => $anomalia->nombre
-      ));
-    }
-
-    $resultados = Resultados::where('estado', '=', '1')->get();
-    foreach ($resultados as $resultado) {
-      array_push($arrayResultados, (object) array(
-        'id' => $resultado->id,
-        'nombre' => $resultado->nombre
-      ));
-    }
-
-    $entidades = EntidadesPagos::all();
-    foreach ($entidades as $entidad) {
-      array_push($arrayEntidades, (object) array(
-        'id' => $entidad->id,
-        'nombre' => $entidad->nombre
+        'nombre' => $anomalia->nombre,
+        'codigo' => $anomalia->codigo
+        'lectura' => $anomalia->lectura
+        'foto' => $anomalia->foto
       ));
     }
 
@@ -119,83 +142,117 @@ class ApiController extends Controller
 
     array_push($arrayFINAL, (object) array(
       'estado' => true,
-      'visitas' => $arrayAvisos,
       'anomalias' => $arrayAnomalias,
-      'resultados' => $arrayResultados,
-      'entidades' => $arrayEntidades,
-      'observaciones_rapidas' => $arrayObservacionesRapidas
+      'observaciones_rapidas' => $arrayObservacionesRapidas,
+      'auditorias' => $arrayAuditorias,
+      'pci' => $arrayPci
     ));
     $collection = new Collection($arrayFINAL);
     return $collection;
   }
 
-  public function actualizarAviso(Request $request)
+  public function actualizarAuditoria(Request $request)
   {
     $response = null;
     if($request->user){
-      $aviso = Avisos::where('id', '=', $request->id)->where('estado', '=', '1')->first();
-      if(isset($aviso->id)){
-        if($request->resultado == 0){
-          $request->resultado = null;
-        }
+      $auditoria = Auditoria::where('id', '=', $request->id)->where('estado', '=', '1')->first();
+      if(isset($auditoria->id)){
         if($request->anomalia == 0){
           $request->anomalia = null;
-        }
-        if($request->entidad_recaudo == 0){
-          $request->entidad_recaudo = null;
         }
         if($request->observacion_rapida == 0){
           $request->observacion_rapida = null;
         }
-        if($request->fecha_compromiso != ""){
-          $request->fecha_compromiso = Carbon::createFromFormat('d/m/Y', $request->fecha_compromiso)->format('Y-m-d');
-        } else if($request->fecha_compromiso == ""){
-          $request->fecha_compromiso = null;
-        }
-        if($request->fecha_pago != ""){
-          $request->fecha_pago = Carbon::createFromFormat('d/m/Y', $request->fecha_pago)->format('Y-m-d');
-        } else if($request->fecha_pago == ""){
-          $request->fecha_pago = null;
-        }
-        $aviso->resultado_id = $request->resultado;
-        $aviso->anomalia_id = $request->anomalia;
-        $aviso->entidad_recaudo_id = $request->entidad_recaudo;
-        $aviso->fecha_pago = $request->fecha_pago;
-        $aviso->fecha_compromiso = $request->fecha_compromiso;
-        $aviso->persona_contacto = $request->persona_contacto;
-        $aviso->cedula = $request->cedula;
-        $aviso->titular_pago = $request->titular_pago;
-        $aviso->telefono = $request->telefono;
-        $aviso->correo_electronico = $request->correo_electronico;
-        $aviso->observacion_rapida = $request->observacion_rapida;
-        $aviso->lectura = $request->lectura;
-        $aviso->observacion_analisis = $request->observacion_analisis;
-        $aviso->latitud = $request->latitud;
-        $aviso->longitud = $request->longitud;
-        $aviso->fecha_recibido = $request->fecha_realizado;
-        $aviso->fecha_recibido_servidor = Carbon::now();
-        $aviso->estado = 2;
-        $aviso->orden_realizado = $request->orden_realizado;
+        $auditoria->anomalia_id = $request->anomalia;
+        $auditoria->observacion_rapida = $request->observacion_rapida;
+        $auditoria->lectura = $request->lectura;
+        $auditoria->observacion_analisis = $request->observacion_analisis;
+        $auditoria->latitud = $request->latitud;
+        $auditoria->longitud = $request->longitud;
+        $auditoria->fecha_recibido = $request->fecha_realizado;
+        $auditoria->fecha_recibido_servidor = Carbon::now();
+        $auditoria->estado = 2;
+        $auditoria->orden_realizado = $request->orden_realizado;
 
-        $aviso->save();
+        $auditoria->save();
 
         try {
           $logSeg = new Log();
           $logSeg->log = '' . $request;
-          $logSeg->aviso_id = $aviso->id;
+          $logSeg->servicio_id = $auditoria->id;
           $logSeg->save();
 
           if($request->foto != null || $request->foto != ""){
             //decode base64 string
             $image = base64_decode($request->foto);
 
-            $archivo = $aviso->id . '.png';
-            \File::put(config('myconfig.ruta_fotos') . $archivo, $image);
+            $archivo = $auditoria->id . '.png';
+            \File::put(config('myconfig.ruta_fotos_auditoria') . $archivo, $image);
           }
         } catch (\Exception $e) {
           $logSeg = new Log();
           $logSeg->log = '' . $e;
-          $logSeg->aviso_id = $aviso->id;
+          $logSeg->servicio_id = $auditoria->id;
+          $logSeg->save();
+        } finally {
+
+        }
+
+        $response = array(
+          'estado' => true
+        );
+      } else {
+        $response = array(
+          'estado' => true
+        );
+      }
+    } else {
+      $response = array(
+        'estado' => false
+      );
+    }
+
+    return $response;
+  }
+
+  public function actualizarPci(Request $request)
+  {
+    $response = null;
+    if($request->user){
+      $pci = Pci::where('id', '=', $request->id)->where('estado', '=', '1')->first();
+      if(isset($pci->id)){
+        if($request->anomalia == 0){
+          $request->anomalia = null;
+        }
+        $pci->anomalia_id = $request->anomalia;
+        $pci->lectura = $request->lectura;
+        $pci->observacion_analisis = $request->observacion_analisis;
+        $pci->latitud = $request->latitud;
+        $pci->longitud = $request->longitud;
+        $pci->fecha_recibido = $request->fecha_realizado;
+        $pci->fecha_recibido_servidor = Carbon::now();
+        $pci->estado = 2;
+        $pci->orden_realizado = $request->orden_realizado;
+
+        $pci->save();
+
+        try {
+          $logSeg = new Log();
+          $logSeg->log = '' . $request;
+          $logSeg->servicio_id = $pci->id;
+          $logSeg->save();
+
+          if($request->foto != null || $request->foto != ""){
+            //decode base64 string
+            $image = base64_decode($request->foto);
+
+            $archivo = $pci->id . '.png';
+            \File::put(config('myconfig.ruta_fotos_pci') . $archivo, $image);
+          }
+        } catch (\Exception $e) {
+          $logSeg = new Log();
+          $logSeg->log = '' . $e;
+          $logSeg->servicio_id = $pci->id;
           $logSeg->save();
         } finally {
 
--- config/myconfig.php
+++ config/myconfig.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-  'ruta_fotos' => 'C:/wamp64/www/control/operativa/storage/app/public/fotos/',
+  'ruta_fotos_auditoria' => 'C:/wamp64/www/control/operativa/storage/app/public/fotos/auditoria/',
+  'ruta_fotos_pci' => 'C:/wamp64/www/control/operativa/storage/app/public/fotos/pci/',
   //'ruta_fotos' => '/var/www/html/operativa/storage/app/public/fotos/'
-  'public_fotos' => 'http://localhost/control/operativa/public/fotos/',
+  'public_fotos_auditoria' => 'http://localhost/control/operativa/public/fotos/auditoria/',
+  'public_fotos_pci' => 'http://localhost/control/operativa/public/fotos/pci/',
 ];
