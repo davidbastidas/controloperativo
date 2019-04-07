1d95e836198b5d280cdbc5246478e35646365048
Ubuntu <ubuntu@ip-172-31-41-237.us-east-2.compute.internal>
2019-04-07 17:07:03 +0000

    local composer

--- config/myconfig.php
+++ config/myconfig.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-  'ruta_fotos_auditoria' => 'C:/wamp64/www/control/operativa/storage/app/public/fotos/auditoria/',
-  'ruta_fotos_pci' => 'C:/wamp64/www/control/operativa/storage/app/public/fotos/pci/',
-  //'ruta_fotos' => '/var/www/html/operativa/storage/app/public/fotos/'
-  'public_fotos_auditoria' => 'http://localhost/control/operativa/public/fotos/auditoria/',
-  'public_fotos_pci' => 'http://localhost/control/operativa/public/fotos/pci/',
+  'ruta_fotos_auditoria' => '/var/www/html/controloperativo/storage/app/public/fotos/auditoria/',
+  'ruta_fotos_pci' => '/var/www/html/controloperativo/storage/app/public/fotos/pci/',
+  //'ruta_fotos' => '/var/www/html/controloperativo/storage/app/public/fotos/'
+  'public_fotos_auditoria' => 'http://52.14.94.46/controloperativo/public/fotos/auditoria/',
+  'public_fotos_pci' => 'http://52.14.94.46/controloperativo/public/fotos/pci/',
   'nombre_app' => 'Lectura de Medidores 1.1',
 ];
